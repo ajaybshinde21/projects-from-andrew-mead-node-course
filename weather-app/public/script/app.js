fc45ce550426403019753f17aec2af8fe0b177df
ajaybshinde21 <ajayshinde1@protonmail.com>
2021-12-26 15:25:42 +0530

    Initial Commit | Create homepage

---
+++ weather-app/public/script/app.js
@@ -0,0 +1,35 @@
+const formNode = document.querySelector("form");
+const cityInputNode = document.querySelector("#city-input");
+const resultNode = document.querySelector("#result");
+formNode.addEventListener("submit", getWeather);
+
+function getWeather(e) {
+  e.preventDefault();
+  const cityName = cityInputNode.value;
+  cityInputNode.value = "";
+  fetch(`/api/weather/?cityName=${cityName}`)
+    .then((res) => {
+      if (res.status == 200) {
+        return res.json();
+      }
+    })
+    .then((res) => {
+      const data = res;
+      if (data.success == false) {
+        resultNode.innerHTML = "<h2>City Not found</h2>";
+      } else {
+        resultNode.innerHTML = `<h2>Result</h2>
+        <p class="result-text" >City Name:${data.location.name}</p>
+      <p class="result-text"> Country:${data.location.country}</p>
+      <div class="temprature">
+            <p class="result-text">temprature :${data.current.temperature} <sup>o</sup>c</p><img src="${data.current.weather_icons[0]}" >
+           
+      </div>
+      <p class="result-text">weather descriptions :${data.current.weather_descriptions[0]}</p>
+      <p class="result-text">it is ${data.current.temperature} <sup>o</sup>c but feels ${data.current.feelslike} <sup>o</sup>c</p>`;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
---
+++ weather-app/src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const request = require("postman-request");
+const { response } = require("express");
+const viewsPath = path.join(__dirname, "../templates", "views");
+const partialsPath = path.join(__dirname, "../templates", "partials");
+const publicPath = path.join(__dirname, "../public");
+const app = express();
+app.use(express.static(publicPath));
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+app.get("", (req, res) => {
+  res.render("index");
+});
+app.get("/api/weather/*", (req, res) => {
+  const cityName = req.query.cityName;
+  const uri = `http://api.weatherstack.com/current?access_key=c39960075c97a1a337d5cbda4c0e43ec&query=${cityName}`;
+  request(uri, (err, response, body) => {
+    if (response.statusCode == 200) {
+      res.json(JSON.parse(body));
+    } else {
+      res.err(404);
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,() => {
+  console.log(`Server started at port :${PORT}`);
+});
